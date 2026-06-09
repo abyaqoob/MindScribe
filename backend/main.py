eae999b65fab1420d7de521900a906b7ca670e16
Abdullah Yaqoob <abdullahyaqoob051@gmail.com>
2026-06-09 17:37:41 +0500

    Fix: Dynamically toggle allow_credentials to support wildcard CORS origins

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.BACKEND_CORS_ORIGINS,
-    allow_credentials=True,
+    allow_credentials="*" not in settings.BACKEND_CORS_ORIGINS,
     allow_methods=["*"],
     allow_headers=["*"],
 )
